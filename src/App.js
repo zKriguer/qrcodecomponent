f14cbfa81e5995d32e116808ae41c529d85eda63
zKriguer <kriguerfreitas2020@gmail.com>
2022-05-21 15:12:55 -0300

    Upload 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import qrcode from './images/image-qr-code.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="qrcode"src={qrcode}/>
+      <h1> Improve your front-end skills by building projects </h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
   );
 }
